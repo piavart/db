3c9b953258764acee780b3b02be61ebb601d3bf3
Арсений <piavarts@gmail.com>
2018-04-08 01:28:31 +0300

    bbn

--- app/Http/Controllers/VisitController.php
+++ app/Http/Controllers/VisitController.php
@@ -16,13 +16,21 @@ class VisitController extends Controller
 
     public function webhook(){
         //$visit = Visit::find(1);
+        $bots = [
+            'YandexBot',
+            'AhrefsBot'
+            ];
+
         $data = $_POST;
-        $visit = new Visit();
-        $visit->ip = $data['REMOTE_ADDR'];//;
-        $visit->extend = $data;
-        $visit->url = $data['HTTP_HOST'].$data['REQUEST_URI'];
-        $visit->http_referer = $data['HTTP_REFERER'];
-        $visit->user_agent = $data['HTTP_USER_AGENT'];
-        $visit->save();
+        if( !preg_match("/bot/i", $data['HTTP_USER_AGENT'] ) ){
+            $visit = new Visit();
+            $visit->ip = $data['REMOTE_ADDR'];//;
+            $visit->extend = $data;
+            $visit->url = $data['HTTP_HOST'].$data['REQUEST_URI'];
+            $visit->http_referer = $data['HTTP_REFERER'];
+            $visit->user_agent = $data['HTTP_USER_AGENT'];
+            $visit->save();
+        }
+
     }
 }
