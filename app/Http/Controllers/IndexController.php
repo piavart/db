dad3e93f518fbd8c77a5ce2e369592f9cb02d031
Арсений <piavarts@gmail.com>
2018-04-07 23:20:11 +0300

    bbn

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\models\Visit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller
 {
     public function index(){
-        dd(phpversion());
+        $visits = Visit::all();
+        dd($visits);
     }
 }
