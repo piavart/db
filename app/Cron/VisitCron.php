da4b996671a703a8ac9e11cd1452dea7d995b8c5
Арсений <piavarts@gmail.com>
2018-04-08 00:13:11 +0300

    bbn

---
+++ app/Cron/VisitCron.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Арсений
+ * Date: 07.04.2018
+ * Time: 23:54
+ */
+
+namespace App\Cron;
+
+
+use App\models\Visit;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class VisitCron extends Command
+{
+
+    protected $signature = 'VisitCron:go';
+
+    public function handle(){
+        $visits = Visit::all();
+        foreach ($visits as $visit){
+            if($visit->ip == '12345'){
+                $visit->ip = '12';
+                $visit->save();
+            }
+        }
+    }
+}
