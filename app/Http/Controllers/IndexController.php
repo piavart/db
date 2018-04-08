12b4dcfe748c91efde582ef1df257830cdca58a8
Арсений <piavarts@gmail.com>
2018-04-08 08:38:39 +0300

    bbn

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -10,7 +10,7 @@ use Illuminate\View\View;
 class IndexController extends Controller
 {
     public function index(){
-        $visits = Visit::all()->orderBy('name', 'desc');
+        $visits = Visit::orderBy('name', 'desc')->get();
         //dd($visits);
         return view('index', ['visits' => $visits]);
     }
