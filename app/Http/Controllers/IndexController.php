e8e105a944c982bc03814627979774f16a5f3e6e
Арсений <piavarts@gmail.com>
2018-04-08 08:38:58 +0300

    bbn

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -12,6 +12,7 @@ class IndexController extends Controller
     public function index(){
         $visits = Visit::orderBy('name', 'desc')->get();
         //dd($visits);
+
         return view('index', ['visits' => $visits]);
     }
 }
