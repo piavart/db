cfcb4d60a976f00a3366b574e724de990843975e
Арсений <piavarts@gmail.com>
2018-04-08 08:42:56 +0300

    bbn

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,9 +3,10 @@
 @section('content')
 
     @foreach ( $visits as $visit)
+        {{ $visit->id }}<br>
         {{ $visit->ip }}<br>
         {{ $visit->user_agent }}<br>
-
+        {{ var_dump( $visit->extend ) }}
         <br><br>
     @endforeach
 
